543648ee0d19462d75a735d0dcdda776a88a34c5
LauraLUK <laura.lodder@uky.edu>
2018-02-24 15:20:03 -0500

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Welcome!  Your email is:  <?php echo $_POST["email"]; ?><br>
+Your password: <?php echo $_POST["password"]; ?><br>
+Your role: <?php echo $_POST["role"]; ?><br>
+
+</body>
+</html>
