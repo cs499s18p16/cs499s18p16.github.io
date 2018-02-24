ed8c4a22311be74d04faf6ecb0e8afd3ff4b392b
LauraLUK <laura.lodder@uky.edu>
2018-02-24 15:18:03 -0500

    Update login.php

--- login.php
+++ login.php
@@ -12,7 +12,7 @@
         <div class="box">
             <h2>Login</h2>
 
-            <form target="_blank"> <!- form result == open a new [blank] browser tab ->
+            <form action="welcome.php" method="post"> <!- form result == open a new [blank] browser tab ->
                 <div class="inputBox">
                     <input type="text" name="" required="">
                     <label>Email</label>
@@ -22,6 +22,17 @@
                         <input type="password" name="" required="">
                         <label>Password</label>
                 </div>
+                
+                <p style="padding: 0 0 2em 0">
+                    
+                <select name="Role">
+                    <option value="Admin">Admin</option>
+                    <option value="Scrum Master">Scrum Master</option>
+                    <option value="Product Owner">Product Owner</option>
+                    <option value="Team Member">Team Member</option>
+                </select>
+                
+                </p>
 
                 <input type="submit" name="" required="submit"> <!- button ->
 
