2a439c6fb83562c4c024ff8b44281156693625db
robcala14 <robert.cala7@gmail.com>
2018-02-24 14:30:25 -0500

    login

---
+++ login.php
@@ -0,0 +1,34 @@
+<?php
+
+//HTML code for website
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Toyota Scrum Tool</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+        <div class="box">
+            <h2>Login</h2>
+
+            <form target="_blank"> <!- form result == open a new [blank] browser tab ->
+                <div class="inputBox">
+                    <input type="text" name="" required="">
+                    <label>Email</label>
+                </div>
+
+                <div class="inputBox">
+                        <input type="password" name="" required="">
+                        <label>Password</label>
+                </div>
+
+                <input type="submit" name="" required="submit"> <!- button ->
+
+            </form>
+        </div>
+    </body>
+
+
+</html>
+ ?>
