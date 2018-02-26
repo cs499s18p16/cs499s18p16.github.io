c323e01077e4772b4981702c99405ef3611c69cc
robcala14 <robert.cala7@gmail.com>
2018-02-26 01:54:24 -0500

    week update + php files

--- login.php
+++ login.php
@@ -1,6 +1,4 @@
-<?php
-
-//HTML code for website
+<!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
@@ -12,26 +10,26 @@
         <div class="box">
             <h2>Login</h2>
 
-            <form action="welcome.php" method="post"> <!- form result == open a new [blank] browser tab ->
+            <form method="POST" action="#">
                 <div class="inputBox">
                     <input type="text" name="" required="">
-                    <label>Email</label>
+                    <label>Username</label>
                 </div>
 
                 <div class="inputBox">
                         <input type="password" name="" required="">
                         <label>Password</label>
                 </div>
-                
+
                 <p style="padding: 0 0 2em 0">
-                    
+
                 <select name="Role">
                     <option value="Admin">Admin</option>
                     <option value="Scrum Master">Scrum Master</option>
                     <option value="Product Owner">Product Owner</option>
                     <option value="Team Member">Team Member</option>
                 </select>
-                
+
                 </p>
 
                 <input type="submit" name="" required="submit"> <!- button ->
@@ -42,4 +40,33 @@
 
 
 </html>
+
+
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$role = "";
+$db = "scrum";
+
+//create connection
+
+mysql_connect($host,$username,$password);
+mysql_select_db($db);
+
+if(isset(['username'])){
+  $username=$_POST['username'];
+  $password=$POST['password']
+
+  $sql="select * from login form where user ='".$username."'AND password ='".$password"' limit 1";
+  $result = mysql_query($sql);
+  if(mysql_num_rows($result ==1)){
+    echo "You have successfully logged in.";
+    exit();
+  }
+  else{
+    echo"Unauthorized credentials were entered, please try again.";
+  }
+}
+
  ?>
