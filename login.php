9dc20f7a90d8f992e70157c6a69a792021bdd9a1
emma <ewnjuguna@Emmas-MacBook-Pro.local>
2018-03-26 11:38:55 -0400

    sprint - 3/26

--- login.php
+++ login.php
@@ -29,13 +29,7 @@
         </div>
     </body>
 
-
 </html>
-<<<<<<< HEAD
-    
- ?>
-=======
-
 
 <?php
 $host = "localhost";
@@ -56,4 +50,4 @@ while($row = mysql_fetch_array($result)){
   }
 }
 ?>
->>>>>>> 4d43b7c9cbce1ce5f2c46a9a543a8ba56c5dfda9
+
