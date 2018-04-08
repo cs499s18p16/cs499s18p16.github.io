e6865ab745e574f936ab66fd654ca3062d306c59
robcala14 <robert.cala7@gmail.com>
2018-04-08 19:20:34 -0400

    pt 3

--- project/index.php
+++ project/index.php
@@ -4,18 +4,19 @@
 
 
 <!DOCTYPE html>
+<!-- HTML code for the login page of the web app-->
 <html>
     <head>
         <meta charset="utf-8">
         <title>Toyota Scrum Tool</title>
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="style.css"> <!--Linking the CSS style page created for the login page-->
     </head>
 
     <body>
         <div class="box">
             <h2>Login</h2>
 
-            <form method="POST" action="login.php">
+            <form method="POST" action="login.php"> <!-- running the login code to redirect to page(s) once credentials have been entered and verified to be correct or not-->
                 <div class="inputBox">
                     <input type="text" name="username" required="">
                     <label>Username</label>
