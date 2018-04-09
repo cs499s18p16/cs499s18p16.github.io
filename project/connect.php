f82dc60a27a0ba65b3bc40cc126f2fb26dc328d9
Poe <justin.poe94@gmail.com>
2018-04-09 15:12:24 -0400

    Cleaned an unnecessary part of connect.php

--- project/connect.php
+++ project/connect.php
@@ -17,8 +17,4 @@ $connection = mysqli_connect($host,$username,$password,$db);
 if (!$connection){
   die("Database Connection Failed:" . mysqli_connect_error()); // conditional statement to ensure successful connection between web app and database hosted on multilab servers
 }
-//$select_db = mysqli_select_db($connection, $db);
-//if(!$select_db){
-//  die("Database Selection Failed" . mysqli_error($connection));
-//}
 ?>
