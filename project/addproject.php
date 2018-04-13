b50941de842fd1274982edd870ca6a45d1b1b5c8
Laura Lodder <laura.lodder@uky.edu>
2018-04-13 13:07:08 -0400

    Fixed addproject.php.  Now correctly adds projects to the database.

--- project/addproject.php
+++ project/addproject.php
@@ -4,12 +4,16 @@
      Purpose: Fetches new project name from getProjectName.php and inserts it into 'projects' table in database as the attribute 'PID'.
      * */
     require 'connect.php';
+    session_start();
 
     $projectName = $_POST['projectname']; //project name from getProjectName.php
-    $username = $_SESSION['user']['email']; //username of PO
-    $sql = "INSERT INTO projects (PID, PO) VALUES ('$projectName', '$username')"; //will insert project into project table in database
+    $uid = $_SESSION['user']['UID']; //username of PO
+    $sql = "INSERT INTO projects (PID, PO) VALUES ('$projectName', '$uid')"; //will insert project into project table in database
 
-    $result = mysqli_query($connection, $sql);
-    $header("Location: productOwner.php"); //redirects user back to the PO homepage after creating a new project
-
+    if(!$result = $connection->query($sql)){
+        echo "$connection->error";
+    }
+    else{
+        header("Location: productOwner.php"); //redirects user back to the PO homepage after creating a new project
+    }
 ?>
