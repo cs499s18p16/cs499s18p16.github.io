3970ed776bd8b98dc39daf4eecfec60ac27f3116
emma <ewnjuguna@Emmas-MacBook-Pro-7.local>
2018-04-18 12:52:32 -0400

    availability page styling

--- project/availability.php
+++ project/availability.php
@@ -11,56 +11,78 @@
 
 <!DOCTYPE html>
 <html>
+
 <head>
   <meta charset="utf-8">
-  <select>
-    <option value="week1">Week 1</option>
-    <option value="week2">Week 2</option>
-    <option value="week3">Week 3</option>
-  </select>
+
   <title>Work Availability</title>
+  <link rel="stylesheet" href="navbar.css">
+
   <style>
-    table, th, td {
-    border: 1px solid black;
-    border-collapse: collapse;
-}
-th, td {
-    padding: 5px;
-}
-th {
-    text-align: left;
-}
-</style>
+    /*table, th, td {
+      border: 1px solid black;
+      border-collapse: collapse;
+    }*/
+    th, td {
+        padding: 5px;
+    }
+    th {
+        text-align: left;
+    }
+  </style>
+
 </head>
-<body>
 
-  <table style="width:100%">
-  <tr>
-    <th>Monday</th>
-    <th>Tuesday</th>
-    <th>Wednesday</th>
-    <th>Thursday</th>
-    <th>Friday</th>
-  </tr>
-  <tr>
-    <td>
-        <input type="text"  name="M_hours" value="" />
-     </td>
-     <td>
-        <input type="text"  name="T_hours" value="" />
-     </td>
-     <td>
-        <input type="text"  name="W_hours" value="" />
-     </td>
-     <td>
-        <input type="text"  name="TR_hours" value="" />
-     </td>
-     <td>
-        <input type="text"  name="F_hours" value="" />
-     </td>
-  </tr>
 
-</table>
+<body background="bridge.jpg">
+
+  <div class="navbar">
+    <div class="dropdown">
+      <button class="dropbtn">My Account<i class="fa fa-caret-down"></i></button>
+      <div class="dropdown-content">
+        <a href="#">Profile</a>
+        <a href="logout.php">Log Out</a>
+      </div>
+    </div>
+     <a href="availability.php">Availability</a>
+     <a href="teamMember.php">Home</a>
+  </div>
+
+  <section class="content" style="margin-top: 70px">
+    <select>
+      <option value="week1">Week 1</option>
+      <option value="week2">Week 2</option>
+      <option value="week3">Week 3</option>
+    </select>
+    <table border=1px width=75% align=center bgcolor="#101721" style="width:100%; color: #ffffff">
+    <tr>
+      <th>Monday</th>
+      <th>Tuesday</th>
+      <th>Wednesday</th>
+      <th>Thursday</th>
+      <th>Friday</th>
+    </tr>
+    <tr>
+      <td>
+          <input type="text"  name="M_hours" value="" />
+      </td>
+      <td>
+          <input type="text"  name="T_hours" value="" />
+      </td>
+      <td>
+          <input type="text"  name="W_hours" value="" />
+      </td>
+      <td>
+          <input type="text"  name="TR_hours" value="" />
+      </td>
+      <td>
+          <input type="text"  name="F_hours" value="" />
+      </td>
+    </tr>
+
+    </table>
+
+  </section>
 
 </body>
 
